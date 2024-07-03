b79e2309fc5201a7502e8a2d4f0e8fd155b5fc17
MINNAKOV <minakov.nikita.1997@mail.ru>
2024-07-03 21:39:48 +0300

    Add files via upload

---
+++ modul 2 2.py
@@ -0,0 +1,12 @@
+first = 20
+second = 30
+third = 40
+if first==second==third:
+    print(3)
+if first!=second==third and first==second!=third:
+    print(2)
+if first!=second!=third:
+    print(0)
+
+
+
